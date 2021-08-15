fc5ab7253336318e3b38eeb06b7ddd1fa41054ff
rdeswardt <rdeswardt@gmail.com>
2021-08-15 23:43:19 +0400

    Database Project

---
+++ StuddyBuddy/StudyBuddyDB/dbo/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] NVARCHAR(MAX) NOT NULL, 
+    [Firstname] NVARCHAR(50) NOT NULL, 
+    [Lastname] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL,
+    [Password] NVARCHAR(MAX) NOT NULL,
+    [Active] BIT NOT NULL DEFAULT 1, 
+  
+)
