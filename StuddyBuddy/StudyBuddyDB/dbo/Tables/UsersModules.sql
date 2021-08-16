7ec31fef4190e11b59f2ca5ea5fe688f9e4597ff
rdeswardt <rdeswardt@gmail.com>
2021-08-16 14:51:12 +0400

    Database Update to tables and ERD diagram

---
+++ StuddyBuddy/StudyBuddyDB/dbo/Tables/UsersModules.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UsersModules]
+(
+	[UniqueId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [ModuleId] INT NOT NULL, 
+    CONSTRAINT [FK_UsersModules_Users] FOREIGN KEY (UserId) REFERENCES Users(UniqueId), 
+    CONSTRAINT [FK_UsersModules_Modules] FOREIGN KEY (ModuleId) REFERENCES Modules(UniqueId)
+)
