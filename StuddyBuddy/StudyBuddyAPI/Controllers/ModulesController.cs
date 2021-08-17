259cb08a6fa6a83d9be8ca90715d6d4498c686dc
rdeswardt <rdeswardt@gmail.com>
2021-08-18 00:12:22 +0400

    Module and Task controller init

---
+++ StuddyBuddy/StudyBuddyAPI/Controllers/ModulesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using StudyBuddyAPI.Models;
+using StudyBuddyAPI.Repository;
+
+
+namespace StudyBuddyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModuleController : ControllerBase
+    {
+        readonly IStudyBuddyRepository studybuddyRepository;
+
+        private readonly ILogger<UserController> _logger;
+
+        public ModuleController(ILogger<UserController> logger, IStudyBuddyRepository _studyBuddyRepository)
+        {
+            _logger = logger;
+            studybuddyRepository = _studyBuddyRepository;
+        }
+
+        [HttpGet]
+        [Route("GetModules")]
+        public IActionResult GetModules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StuddyBuddy/StudyBuddyAPI/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using StudyBuddyAPI.Models;
+using StudyBuddyAPI.Repository;
+
+
+namespace StudyBuddyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        readonly IStudyBuddyRepository studybuddyRepository;
+
+        private readonly ILogger<UserController> _logger;
+
+        public TaskController(ILogger<UserController> logger, IStudyBuddyRepository _studyBuddyRepository)
+        {
+            _logger = logger;
+            studybuddyRepository = _studyBuddyRepository;
+        }
+
+        [HttpGet]
+        [Route("GetModules")]
+        public IActionResult GetTasks()
+        {
+            return Ok();
+        }
+    }
+}
