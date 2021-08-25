a7699cad80a47bb6f2d0853154e7b7b5458f4728
rdeswardt <rdeswardt@gmail.com>
2021-08-25 20:34:55 +0400

    Module comments

--- StuddyBuddy/StudyBuddyAPI/Repository/StudyBuddyRepository.cs
+++ StuddyBuddy/StudyBuddyAPI/Repository/StudyBuddyRepository.cs
@@ -105,7 +105,7 @@ namespace StudyBuddyAPI.Repository
                     db.SaveChanges();
 
                     //Add role to user
-                    db.UserRoles.Add(new UserRole {RoleId = 1, UserId = user.UniqueId});
+                    db.UserRoles.Add(new UserRole { RoleId = 1, UserId = user.UniqueId });
                     db.SaveChanges();
                 }
 
@@ -156,42 +156,68 @@ namespace StudyBuddyAPI.Repository
             return null;
         }
 
+        /// <summary>
+        /// Get Moduoles a user had added
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         public List<Module> GetModulesPerUser(string username)
         {
-            if (db != null) 
+            if (db != null)
             {
-
                 var modules = db.UsersModules.Where(x => x.UserId == db.Users.FirstOrDefault(u => u.Username == username).UniqueId).Select(y => y.Module).ToList();
 
-               
                 return modules;
-               
-                
             }
 
             return null;
         }
 
+        /// <summary>
+        /// Get Module by Name
+        /// </summary>
+        /// <param name="moduleName"></param>
+        /// <returns></returns>
         public Module GetModuleByName(string moduleName)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get Module by Unique ID
+        /// </summary>
+        /// <param name="uniqueId"></param>
+        /// <returns></returns>
         public Module GetModuleByUniqueId(int uniqueId)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Insert Module
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
         public Module InsertModule(Module module)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Update Modules
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
         public Module UpdateModule(Module module)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Remove Module
+        /// </summary>
+        /// <param name="uniqueId"></param>
+        /// <returns></returns>
         public bool RemoveModule(int uniqueId)
         {
             throw new NotImplementedException();
@@ -200,26 +226,51 @@ namespace StudyBuddyAPI.Repository
         #endregion
 
         #region Tasks
+        /// <summary>
+        /// Get Task per module
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
         public List<Task> GetTasksPerModule(Module module)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get Task by Id
+        /// </summary>
+        /// <param name="uniqueId"></param>
+        /// <returns></returns>
         public Task GetTaskByUniqueId(int uniqueId)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Insert new Task
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public Task InsertTask(Task task)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Update Taks
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public Task UpdateTask(Task task)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Remove Task
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public bool RemoveTask(Task task)
         {
             throw new NotImplementedException();
